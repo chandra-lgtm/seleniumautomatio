ad901730e78b75264834fb8dba5be5104d00e888
chandra-lgtm <chandu.kiran842@gmail.com>
2024-09-01 12:43:20 +0530

    Added comment in Crat page

--- Testing practices/demo/src/main/java/pages/CartPage.java
+++ Testing practices/demo/src/main/java/pages/CartPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//comment conflict
 public class CartPage {
     WebDriver driver;
     @FindBy(xpath="//a[contains(text(),'Home')]")
